40e9d5a9b13661d72462fc499bd86ca4e7a3b2c0
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2020-03-26 13:40:36 -0400

    put skills in a card

--- src/components/SkillsSection/SkillsContainer.js
+++ src/components/SkillsSection/SkillsContainer.js
@@ -4,21 +4,22 @@ import { skills, Skill } from './index';
 
 export const SkillsContainer = () => {
     return (
-        <div className='skills container'>
+        <>
             <h2 className='skills-header' id='skills'>
                 skills
             </h2>
-
-            {skills.map(skillGroup => (
-                <React.Fragment key = {skillGroup.title}>
-                    <h3>{skillGroup.title}</h3>
-                    <div className='skill-group-container'>
-                        {skillGroup.skills.map(skill => (
-                            <Skill key={skill} data={skill} />
-                        ))}
-                    </div>
-                </React.Fragment>
-            ))}
-        </div>
+            <div className='skills container'>
+                {skills.map(skillGroup => (
+                    <React.Fragment key={skillGroup.title}>
+                        <h3>{skillGroup.title}</h3>
+                        <div className='skill-group-container'>
+                            {skillGroup.skills.map(skill => (
+                                <Skill key={skill} data={skill} />
+                            ))}
+                        </div>
+                    </React.Fragment>
+                ))}
+            </div>
+        </>
     );
 };
